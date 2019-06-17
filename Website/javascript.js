d8c26bee6abbd86449af3cf5e03b5d494f4d123d
kirstenpropitius <31803652+kirstenpropitius@users.noreply.github.com>
2019-06-17 14:11:58 +0200

    Add files via upload

---
+++ Website/javascript.js
@@ -0,0 +1,38 @@
+var foto = document.querySelector('#fotovoor');
+
+foto.addEventListener('click',
+    function () {
+        fotovoor.classList.toggle('opzij');
+    }
+)
+
+
+
+
+//var oog1 = document.querySelector('#oog1');
+//var oog2 = document.querySelector('#oog2');
+//var oog3 = document.querySelector('#oog3');
+//var oog5 = document.querySelector('#oog5');
+//var oog6 = document.querySelector('#oog6');
+//
+//var lijn1 = document.querySelector('.lijn1');
+//var lijn2 = document.querySelector('.lijn2');
+//var lijn3 = document.querySelector('.lijn5');
+//var lijn5 = document.querySelector('.lijn4');
+//var lijn6 = document.querySelector('.lijn3');
+//
+//oog1.addEventListener('click', function () {
+//    lijn1.classList.toggle('lijn-animatie');
+//})
+//oog2.addEventListener('click', function () {
+//    lijn2.classList.toggle('lijn-animatie');
+//})
+//oog3.addEventListener('click', function () {
+//    lijn3.classList.toggle('lijn-animatie');
+//})
+//oog5.addEventListener('click', function () {
+//    lijn5.classList.toggle('lijn-animatie');
+//})
+//oog6.addEventListener('click', function () {
+//    lijn6.classList.toggle('lijn-animatie');
+//})
